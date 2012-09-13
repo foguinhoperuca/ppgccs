225653107b28aef92dfa257d18eee85057c59158
Jefferson Campos <foguinho.peruca@gmail.com>
2012-09-12 21:36:07 -0300

    refs #96430 Correção da licença.

--- master101/cormen/chapter01/insertion_sort/java/InsertionSort/src/org/awknet/ppgccs/insertionSort/InsertionSort.java
+++ master101/cormen/chapter01/insertion_sort/java/InsertionSort/src/org/awknet/ppgccs/insertionSort/InsertionSort.java
@@ -14,7 +14,7 @@
  * GNU General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public License
- * along with awknet-commons. If not, see <http://www.gnu.org/licenses/>.
+ * along with awknet-ppgccs. If not, see <http://www.gnu.org/licenses/>.
  */
 
 package org.awknet.ppgccs.insertionSort;
